be50edebc98213912c9c79fa032d6080891e6cb7
fetaro <fetaro@gmail.com>
2012-10-17 01:06:22 +0900

    test bug fix

--- test/rbatch/test_auto_logger.rb
+++ test/rbatch/test_auto_logger.rb
@@ -1,22 +1,18 @@
 require 'test/unit'
 class LoggerTest < Test::Unit::TestCase
   def setup
+   @log_dir =  "./test/log/"
+    Dir::mkdir(@log_dir) if ! Dir.exists? @log_dir
   end
 
-  def normal
-    require '../lib/rbatch/auto_logger'
+  def test_require
+    require './lib/rbatch/auto_logger'
+  end
+  def test_infolog
+    require './lib/rbatch/auto_logger'
     RBatch::auto_logger do | log |
       log.info("hoge")
-      unko
     end
   end
 end
-
-
-
-    require '../lib/rbatch/auto_logger'
-    RBatch::auto_logger do | log |
-      log.info("hoge")
-      unko
-    end
 
