249ffc1b1f5473cd6820980173bf2de1a741d15b
fetaro <fetaro@gmail.com>
2012-10-17 00:13:20 +0900

    bug fix

--- test/test_runcher.rb
+++ test/test_runcher.rb
@@ -1,40 +1,40 @@
 require 'test/unit'
-require '../lib/runcher'
+require './lib/rbatch/runcher'
 
 class RuncherTest < Test::Unit::TestCase
   def setup
   end
 
-  def cmd_exists
+  def test_cmd_exists
     result = cmd "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 0;'"
     assert_equal "1", result[:stdout].chomp
     assert_equal "2", result[:stderr].chomp
     assert_equal 0, result[:status]
   end
-  def cmd_does_not_exist
+  def test_cmd_does_not_exist
     assert_raise(Errno::ENOENT){
       cmd "not_exist_command"
     }
   end
-  def stdout_size_greater_than_65534
+  def test_stdout_size_greater_than_65534
     result = cmd "ruby -e '100000.times{print 0}'"
     assert_equal 100000, result[:stdout].chomp.size
     assert_equal "", result[:stderr].chomp
     assert_equal 0, result[:status]
   end
-  def stdout_size_greater_than_65534_with_status_1
+  def test_stdout_size_greater_than_65534_with_status_1
     result = cmd "ruby -e '100000.times{print 0}; exit 1'"
     assert_equal 100000, result[:stdout].chomp.size
     assert_equal "", result[:stderr].chomp
     assert_equal 1, result[:status]
   end
-  def status_code_is_1
+  def test_status_code_is_1
     result = cmd "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 1;'"
     assert_equal "1", result[:stdout].chomp
     assert_equal "2", result[:stderr].chomp
     assert_equal 1, result[:status]
   end
-  def status_code_is_greater_than_256
+  def test_status_code_is_greater_than_256
     returncode = 300
     result = cmd  "ruby -e 'STDOUT.print 1; STDERR.print 2; exit #{returncode};'"
     assert_equal "1", result[:stdout].chomp
