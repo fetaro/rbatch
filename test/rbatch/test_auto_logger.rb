f5bac3d0e858b633beb7b68fe2155882bade246c
fetaro <fetaro@gmail.com>
2012-10-17 09:36:08 +0900

    test bug fix

--- test/rbatch/test_auto_logger.rb
+++ test/rbatch/test_auto_logger.rb
@@ -3,14 +3,36 @@ require 'rbatch/auto_logger'
 
 class LoggerTest < Test::Unit::TestCase
   def setup
-   @log_dir =  "./test/log/"
-    Dir::mkdir(@log_dir) if ! Dir.exists? @log_dir
+    @log_dir =  "./test/log/"
+    if Dir.exists? @log_dir
+      Dir::foreach(@log_dir) do |f|
+        File::delete(@log_dir+f) if ! (/\.+$/ =~ f)
+      end
+    else
+      Dir::mkdir(@log_dir)
+    end
+  end
+
+  def teardown
+    if Dir.exists? @log_dir
+      Dir::foreach(@log_dir) do |f|
+        File::delete(@log_dir+f) if ! (/\.+$/ =~ f)
+      end
+      Dir::rmdir(@log_dir)
+    end
   end
 
   def test_infolog
     RBatch::auto_logger do | log |
       log.info("hoge")
     end
+  end
+
+  def test_log_dir_doesnot_exist
+    Dir::rmdir(@log_dir)
+    assert_raise(Errno::ENOENT){
+      RBatch::auto_logger {|log|}
+    }
   end
 end
 
