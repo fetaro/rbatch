f4c2780511a7e79b7abf20926419ad3f20f52338
fetaro <fetaro@gmail.com>
2012-10-19 02:46:50 +0900

    rename cmd

--- lib/rbatch/runcher.rb
+++ lib/rbatch/runcher.rb
@@ -1,27 +1,33 @@
 require 'fileutils'
 require 'tempfile'
-# Wrapper of Kernel#spawn
-#
-#   ex)  result = cmd "ls -l"
-#        p result[:stdout]
-#
-# params : *cmd_params : command string
-#
-# return : {:stdout => stdout , :stderr => stderr, :status => status}
-#
-def cmd(*cmd_params)
-  tmp_dir="."
-  tmp_out = Tempfile::new("tmpout",tmp_dir)
-  tmp_err = Tempfile::new("tmperr",tmp_dir)
-  begin
-    pid = spawn(*cmd_params,:out => [tmp_out,"w"],:err => [tmp_err,"w"])
-    status =  Process.waitpid2(pid)[1] >> 8
-    stdout = File.read(tmp_out)
-    stderr = File.read(tmp_err)
-  ensure
-    [tmp_out,tmp_err].each do |f|
-      f.close(true)
+
+module RBatch
+
+  module_function
+  # Wrapper of Kernel#spawn
+  #
+  #   ex)  result = RBatch::run "ls -l"
+  #        p result[:stdout]
+  #
+  # params : *cmd_params : command string
+  #
+  # return : {:stdout => stdout , :stderr => stderr, :status => status}
+  #
+  def run(*cmd_params)
+    tmp_dir="."
+    tmp_out = Tempfile::new("tmpout",tmp_dir)
+    tmp_err = Tempfile::new("tmperr",tmp_dir)
+    begin
+      pid = spawn(*cmd_params,:out => [tmp_out,"w"],:err => [tmp_err,"w"])
+      status =  Process.waitpid2(pid)[1] >> 8
+      stdout = File.read(tmp_out)
+      stderr = File.read(tmp_err)
+    ensure
+      [tmp_out,tmp_err].each do |f|
+        f.close(true)
+      end
     end
+    return {:stdout => stdout , :stderr => stderr, :status => status}
   end
-  return {:stdout => stdout , :stderr => stderr, :status => status}
+
 end
--- test/rbatch/test_runcher.rb
+++ test/rbatch/test_runcher.rb
@@ -6,37 +6,37 @@ class RuncherTest < Test::Unit::TestCase
   end
 
   def test_cmd_exists
-    result = cmd "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 0;'"
+    result = RBatch::run "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 0;'"
     assert_equal "1", result[:stdout].chomp
     assert_equal "2", result[:stderr].chomp
     assert_equal 0, result[:status]
   end
   def test_cmd_does_not_exist
     assert_raise(Errno::ENOENT){
-      cmd "not_exist_command"
+      RBatch::run "not_exist_command"
     }
   end
   def test_stdout_size_greater_than_65534
-    result = cmd "ruby -e '100000.times{print 0}'"
+    result = RBatch::run "ruby -e '100000.times{print 0}'"
     assert_equal 100000, result[:stdout].chomp.size
     assert_equal "", result[:stderr].chomp
     assert_equal 0, result[:status]
   end
   def test_stdout_size_greater_than_65534_with_status_1
-    result = cmd "ruby -e '100000.times{print 0}; exit 1'"
+    result = RBatch::run "ruby -e '100000.times{print 0}; exit 1'"
     assert_equal 100000, result[:stdout].chomp.size
     assert_equal "", result[:stderr].chomp
     assert_equal 1, result[:status]
   end
   def test_status_code_is_1
-    result = cmd "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 1;'"
+    result = RBatch::run "ruby -e 'STDOUT.print 1; STDERR.print 2; exit 1;'"
     assert_equal "1", result[:stdout].chomp
     assert_equal "2", result[:stderr].chomp
     assert_equal 1, result[:status]
   end
   def test_status_code_is_greater_than_256
     returncode = 300
-    result = cmd  "ruby -e 'STDOUT.print 1; STDERR.print 2; exit #{returncode};'"
+    result = RBatch::run  "ruby -e 'STDOUT.print 1; STDERR.print 2; exit #{returncode};'"
     assert_equal "1", result[:stdout].chomp
     assert_equal "2", result[:stderr].chomp
     case RUBY_PLATFORM
