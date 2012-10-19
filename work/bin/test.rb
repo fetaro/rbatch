6a70ec2757d2931ed81774fa2aad5c6e516dd370
fetaro <fetaro@gmail.com>
2012-10-20 00:41:53 +0900

    delete files

--- work/bin/test.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/local/lib/ruby192/bin/ruby -I /root/rbatch/lib
-
-require 'rbatch/auto_logger'
-
-RBatch::Log::record({:output_dir => "../work"}){|log|
- log.info( "hoge" )
-  #exception_hire
-}
