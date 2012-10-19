ba97f30e9053d8eef37ee8372aea5d876458ff0c
fetaro <fetaro@gmail.com>
2012-10-20 00:06:01 +0900

    test refactoring

--- lib/rbatch/auto_logger.rb
+++ lib/rbatch/auto_logger.rb
@@ -3,9 +3,10 @@ require 'pathname'
 
 module RBatch
   class Log
-    @@default_file_prefix = "%Y%m%d_%H%M%S_"
-    @@default_output_dir  = File.join(File.dirname($0), "..", "log")
     @@debug = false
+
+    def Log.debug=(bol); @@debug = true ; end
+    def Log.debug ; @@debug ; end
 
     #
     # param: option
@@ -14,21 +15,22 @@ module RBatch
     #  :path
     #  :formatter   : input directory to "Logger#formatter= "
     def initialize(opt = nil)
-      file_prefix = @@default_file_prefix
-      output_dir = @@default_output_dir
-      path = nil
-      formatter = nil
+      file_prefix = "%Y%m%d_%H%M%S_"
+      output_dir  = File.join(File.dirname(RBatch.program_name), ".." , "log")
+      path        = nil
+      formatter   = nil
       if ! opt.nil?
         file_prefix = opt[:file_prefix] if opt[:file_prefix]
-        output_dir = opt[:output_dir] if opt[:output_dir]
-        path = opt[:path] if opt[:path]
-        formatter = opt[:formatter] if opt[:formatter]
+        output_dir  = opt[:output_dir]  if opt[:output_dir]
+        path        = opt[:path]        if opt[:path]
+        formatter   = opt[:formatter]   if opt[:formatter]
       end
       if path.nil?
-        file = Time.now.strftime(file_prefix) + Pathname(File.basename($0)).sub_ext(".log").to_s
+        file = Time.now.strftime(file_prefix) + Pathname(File.basename(RBatch.program_name)).sub_ext(".log").to_s
         path = File.join(output_dir,file)
       end
       puts "Logfile Path = " + path if @@debug
+      puts "RBatch.program_name = " + RBatch.program_name if @@debug
       log = Logger.new(path)
       log.formatter = formatter if formatter
       begin
