15c8f439a17084c6948ec0da69ca1aa603f1e0ba
fetaro <fetaro@gmail.com>
2012-10-21 02:42:42 +0900

    make rdoc

--- lib/rbatch/auto_config.rb
+++ lib/rbatch/auto_config.rb
@@ -5,6 +5,20 @@ module RBatch
 
   module_function
 
+  # Read config file and return hash opject.
+  # 
+  # Default config file path is "../config/(Program name).yaml"
+  # ==== Sample
+  # config : ./config/sample2.yaml
+  #  key: value
+  #  array:
+  #   - item1
+  #   - item2
+  #   - item3
+  # script : ./bin/sample2.rb
+  #  require 'rbatch'
+  #  p RBatch::read_config
+  #  => {"key" => "value", "array" => ["item1", "item2", "item3"]}
   def read_config
     file = Pathname(File.basename(RBatch.program_name)).sub_ext(".yaml").to_s
     dir = File.join(File.join(File.dirname(RBatch.program_name),".."),"config")
