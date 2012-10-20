e66396e4d71edb0714e6f974e74539749e95389f
fetaro <fetaro@gmail.com>
2012-10-21 02:42:42 +0900

    make rdoc

--- lib/rbatch/auto_logger.rb
+++ lib/rbatch/auto_logger.rb
@@ -3,19 +3,32 @@ require 'pathname'
 
 module RBatch
   class Log
-    @@debug = false
+    @@verbose = false
 
-    def Log.debug=(bol); @@debug = true ; end
-    def Log.debug ; @@debug ; end
+    # Set verbose mode flag.
+    def Log.verbose=(bol); @@verbose = true ; end
+    # Get verbose mode flag.
+    def Log.verbose ; @@verbose ; end
 
-    #=Auto Loggin Block
+    # Auto Logging Block.
     # 
-    # param: option
-    #  :file_prefix
-    #  :output_dir
-    #  :path
-    #  :formatter   : input directory to "Logger#formatter= "
-    #  :
+    # ==== Sample
+    #  RBatch::Log.new({:level => Logger::INFO}){ |log|
+    #    log.info "info string"
+    #    log.error "error string"
+    #    raise "exception"
+    #  }
+    #
+    # ==== Params
+    # +opt+ = Option hash object. Hash keys is follows.
+    # - +:file_prefix+ (String) = log filename prefix format. Default is "%Y%m%d_%H%M%S_".
+    # - +:output_dir+ (String) = log direcotry path. Default is "../log"
+    # - +:path+ (String) = log file path. Default is {output_dir} + {file_prefix} + (Program name) + ".log"
+    # - +:formatter+ (String) = log formatter. "Logger#formatter= "
+    # - +:level+ (Logger::[DEBUG|INFO|WARN|ERROR|FATAL])= log level. Default is Logger::INFO
+    # 
+    # ==== Block params
+    # +log+ = Instance of +Logger+
     def initialize(opt = nil)
       file_prefix = "%Y%m%d_%H%M%S_"
       output_dir  = File.join(File.dirname(RBatch.program_name), ".." , "log")
@@ -33,8 +46,8 @@ module RBatch
         file = Time.now.strftime(file_prefix) + Pathname(File.basename(RBatch.program_name)).sub_ext(".log").to_s
         path = File.join(output_dir,file)
       end
-      puts "Logfile Path = " + path if @@debug
-      puts "RBatch.program_name = " + RBatch.program_name if @@debug
+      puts "Logfile Path = " + path if @@verbose
+      puts "RBatch.program_name = " + RBatch.program_name if @@verbose
       log = Logger.new(path)
       log.formatter = formatter if formatter
       log.level = level
