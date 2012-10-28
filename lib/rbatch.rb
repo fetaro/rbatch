a53cf7bdaf77ccdd88d3b5df64883a75083e468c
fetaro <fetaro@gmail.com>
2012-10-28 23:43:18 +0900

    file rename

--- lib/rbatch.rb
+++ lib/rbatch.rb
@@ -5,9 +5,17 @@ module RBatch
   module_function
   def program_name=(f) ; @@program_name = f ; end
   def program_name ; @@program_name ; end
+  def common_config
+    path = File.join(File.join(File.dirname(RBatch.program_name),".."),"config","rbatch.yaml")
+    if File.exist?(path)
+      return YAML::load_file(path)
+    else
+      return nil
+    end
+  end
 end
 
-require 'rbatch/auto_logger'
-require 'rbatch/auto_config'
-require 'rbatch/runcher'
+require 'rbatch/log'
+require 'rbatch/config'
+require 'rbatch/cmd'
 
