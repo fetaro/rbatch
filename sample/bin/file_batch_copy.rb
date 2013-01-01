312c29dd6ffde186eaf851d1d5add9019f61dc49
fetaro <fetaro@gmail.com>
2013-01-02 00:24:13 +0900

    enhance sample

--- sample/bin/file_batch_copy.rb
+++ sample/bin/file_batch_copy.rb
@@ -1,6 +1,9 @@
 require 'rbatch'
-
+require 'fileutils'
 RBatch::Log.new do |log|
-  log.info RBatch::config
+  RBatch::config["file_list"].each do | file |
+    log.info("cp -r " + file + " " + RBatch::config["backup_dir"])
+    FileUtils.cp_r(file,RBatch::config["backup_dir"])
+  end
 end
 
