41d7519967c4f33fe8548a85494cc1f9317aec80
fetaro <fetaro@gmail.com>
2013-01-14 11:13:18 +0900

    add config key check

--- lib/rbatch/config.rb
+++ lib/rbatch/config.rb
@@ -5,11 +5,14 @@ module RBatch
 
   module_function
 
+  # Alias of RBatch::Config.new
+  def config ; Config.new end
+
   # Read config file and return hash opject.
   # 
-  # Default config file path is "../config/(Program name).yaml"
+  # Default config file path is "../conf/(Program Base name).yaml"
   # ==== Sample
-  # config : ./config/sample2.yaml
+  # config : ./conf/sample2.yaml
   #  key: value
   #  array:
   #   - item1
@@ -17,13 +20,26 @@ module RBatch
   #   - item3
   # script : ./bin/sample2.rb
   #  require 'rbatch'
-  #  p RBatch::config
+  #  p RBatch::Config.new
+  #  # or  p RBatch::config
   #  => {"key" => "value", "array" => ["item1", "item2", "item3"]}
-  def config
-    file = Pathname(File.basename(RBatch.program_name)).sub_ext(".yaml").to_s
-    dir = File.join(File.join(File.dirname(RBatch.program_name),".."),"conf")
-    return YAML::load_file(File.join(dir,file))
+  class Config
+    @path
+    @config
+    def initialize
+      file = Pathname(File.basename(RBatch.program_name)).sub_ext(".yaml").to_s
+      dir = File.join(File.join(File.dirname(RBatch.program_name),".."),"conf")
+      @path = File.join(dir,file)
+      @config = YAML::load_file(@path)
+    end
+    def[](key)
+      raise RBatch::Config::Exception, "Value of key=\"#{key}\" is nil" if @config[key].nil?
+      @config[key]
+    end
+    def path ; @path ; end
+    def to_s ; @config.to_s ;end
   end
 
+  class RBatch::Config::Exception < Exception; end
 end
 
--- sample/bin/test.rb
+++ sample/bin/test.rb
@@ -1,12 +1,6 @@
 require 'rbatch'
-#RBatch::Log.verbose=true
-RBatch::Log.new({ :quiet => true } ) do |log|
-  log.delete_old_log
-p "--"
-  log.delete_old_log 1
-p "--"
-  log.delete_old_log 2
-p "--"
-  log.delete_old_log 3
 
-end
+require 'rbatch'
+
+p RBatch::config
+p RBatch::config
