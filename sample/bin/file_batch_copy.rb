036a5066ed59c590e9dbdf060a48c2730160f1f5
fetaro <fetaro@gmail.com>
2013-01-21 20:33:42 +0900

    サンプル拡充

--- sample/bin/file_batch_copy.rb
+++ sample/bin/file_batch_copy.rb
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 require 'rbatch'
 require 'fileutils'
+
+# ファイル一括コピー
 RBatch::Log.new do |log|
+  target_dir = RBatch::config["target_dir"]
   RBatch::config["file_list"].each do | file |
-    log.info("cp -r " + file + " " + RBatch::config["backup_dir"])
-    FileUtils.cp_r(file,RBatch::config["backup_dir"])
+    log.info("copy " + file + " -> " + target_dir)
+    FileUtils.cp_r(file,target_dir)
   end
 end
 
