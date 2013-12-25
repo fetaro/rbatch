603e7981034a5350a8c10f0837193eaaebdf1c40
fetaro <fetaro@gmail.com>
2013-12-26 04:59:59 +0900

    test finish

--- spec/run_conf_spec.rb
+++ spec/run_conf_spec.rb
@@ -1,17 +1,15 @@
 require 'tmpdir'
-ENV["RB_HOME"]=Dir.tmpdir
-
-require 'rbatch'
+require 'rbatch/run_conf'
 
 describe RBatch::RunConf do
   before :all do
-    @config = RBatch.run_conf_path
+    @config = File.join(Dir.tmpdir,"run_conf_test.yaml")
   end
 
   before :each do
     FileUtils.rm @config if File.exists? @config
     open( @config  , "w" ){|f| f.write("")}
-    RBatch.reload_run_conf
+    @rc = RBatch::RunConf.new(@config)
   end
 
   after :each do
@@ -21,36 +19,66 @@ describe RBatch::RunConf do
   end
 
   it "is default when run_conf is empty" do
-    expect(RBatch.run_conf[:log_dir]).to eq "log"
+    expect(@rc[:log_dir]).to eq "log"
   end
 
   it "is default when run_conf does not exist" do
     FileUtils.rm @config
-    expect(RBatch.run_conf[:log_dir]).to eq "log"
+    tmp = RBatch::RunConf.new(@config)
+    expect(tmp[:log_dir]).to eq "log"
   end
 
   it "raise when key does not exist" do
-    expect{RBatch.run_conf[:not_exist_key]}.to raise_error(RBatch::RunConf::Exception)
+    expect{@rc[:not_exist_key]}.to raise_error(RBatch::RunConf::Exception)
   end
 
   it "read run conf" do
     open( @config  , "w" ){|f| f.write("log_dir: hoge")}
-    RBatch.reload_run_conf
-    expect(RBatch.run_conf[:log_dir]).to eq "hoge"
+    tmp = RBatch::RunConf.new(@config)
+    expect(tmp[:log_dir]).to eq "hoge"
   end
 
   it "raise when run_conf has unreserved key" do
     open( @config  , "w" ){|f| f.write("unreserved: hoge")}
-    expect{RBatch.reload_run_conf}.to raise_error(RBatch::RunConf::Exception)
+    expect{tmp = RBatch::RunConf.new(@config)}.to raise_error(RBatch::RunConf::Exception)
   end
 
-  it "is set by []= method" do
-    expect(RBatch.run_conf[:log_dir]).to eq "log"
-    RBatch.run_conf[:log_dir] = "hoge"
-    expect(RBatch.run_conf[:log_dir]).to eq "hoge"
+  describe "[]= method" do
+    it "success" do
+      expect(@rc[:log_dir]).to eq "log"
+      @rc[:log_dir] = "hoge"
+      expect(@rc[:log_dir]).to eq "hoge"
+    end
+
+    it "raise when set unreserved key" do
+      expect{@rc[:unreservied] = "hoge"}.to raise_error(RBatch::RunConf::Exception)
+    end
+  end
+  
+  describe "merge! method" do
+    it "success" do
+      opt={ :log_dir => "bar"}
+      @rc.merge!(opt)
+      expect(@rc[:log_dir]).to eq "bar"
+    end
+
+    it "raise when key does not exist" do
+      opt={ :unreserved => "bar"}
+      expect{@rc.merge!(opt)}.to raise_error(RBatch::RunConf::Exception)
+    end
   end
 
-  it "raise when set unreserved key by []= method" do
-    expect{RBatch.run_conf[:unreservied] = "hoge"}.to raise_error(RBatch::RunConf::Exception)
+  describe "merge method" do
+    it "success" do
+      opt={ :log_dir => "bar"}
+      tmp = @rc.merge(opt)
+      expect(@rc[:log_dir]).to eq "log"
+      expect(tmp[:log_dir]).to eq "bar"
+    end
+
+    it "raise when key does not exist" do
+      opt={ :unreserved => "bar"}
+      expect{@rc.merge(opt)}.to raise_error(RBatch::RunConf::Exception)
+    end
   end
 end
