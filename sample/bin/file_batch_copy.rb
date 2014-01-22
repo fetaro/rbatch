585e9fbb61aaac9cea8a739908f69bd2796c75df
fetaro <fetaro@gmail.com>
2014-01-21 19:13:27 -0500

    sample enhance

--- sample/bin/file_batch_copy.rb
+++ sample/bin/file_batch_copy.rb
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 require 'rbatch'
-require 'fileutils'
 
-# ファイル一括コピー
+# File butch copy
 RBatch::Log.new do |log|
+  require 'fileutils'
   target_dir = RBatch::config["target_dir"]
   RBatch::config["file_list"].each do | file |
-    log.info("copy " + file + " -> " + target_dir)
+    log.info("copy " + file + " " + target_dir)
     FileUtils.cp_r(file,target_dir)
   end
 end
--- sample/bin/log_backup.rb
+++ sample/bin/log_backup.rb
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
 require 'rbatch'
-require 'fileutils'
-require 'date'
 
 RBatch::Log.new do |log|
-  log.info("Start---------------")
+  require 'fileutils'
+  require 'date'
   target_dir = RBatch::config["target_dir"]
   RBatch::config["file_list"].each do |file_wildcard|
     Dir::glob(file_wildcard).each do |file|
       if ! File.exists?(File.join(target_dir,File.basename(file)))
-        log.info("Copy " + file + " to " + target_dir)
+        log.info("Copy " + file + " " + target_dir)
         FileUtils.cp(file,target_dir)
       else
         log.info("Skip " + file + " (already backuped)")
